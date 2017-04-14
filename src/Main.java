75cd82bc604132fffb52a70a0eab2dfd37acca7f
mdbubb <mdb17@thebubbs.net>
2017-04-13 20:34:58 -0400

    gotta add them up now

--- src/Main.java
+++ src/Main.java
@@ -12,47 +12,46 @@ import java.util.regex.*;
  * Created by mdb17 on 4/12/2017.
  */
 public class Main {
-    private static ArrayList<Integer> a = new ArrayList<>();
-
-    private static int secretNum = 0;
+    public static String left;
+    public static String id;
+    public static String s;
 
     public static void main(String[] args) throws IOException {
         Scanner raw = new Scanner(new File("raw"));
-        int index = 0;
-
-        String line[] = new String[56];
         raw.nextLine();
         while (raw.hasNext()) {
             try {
-                raw.useDelimiter("\\+");
-                System.out.println(raw.next());
-                raw.useDelimiter("\\s+");
-               // System.out.println(raw.next());
-            }
-            catch (NoSuchElementException e){
+                left = raw.nextLine().replaceAll("[^0123456789.+]", "");
+                char p[] = left.toCharArray();
+                try {
+                    id = "";
+                    if (Character.isDigit(p[0])) {
+                        if (Character.isDigit(p[1])) {
+                            id = (p[0] + "" + p[1]);
+                        } else {
+                            id = (p[0] + "");
+                        }
+                    }
+                    StringBuilder d = new StringBuilder(left);
+
+                    if (id.length() == 1) {
+                        d.delete(0, 1);
+                        s = d.toString();
+                    } else {
+                        d.delete(0, 2);
+                        s = d.toString();
+
+                    }
+
+                    System.out.println(s);
+                    //System.out.println(left);
+                } catch (IndexOutOfBoundsException e) {
+                    raw.nextLine();
+                }
+            } catch (NoSuchElementException e) {
                 System.out.println();
             }
-        }
-
-
-        while (raw.hasNextLine()) {
-
-            try {
-                //Secret Number into 'a' arrayList
-                secretNum = raw.nextInt();
-                boolean containsSecretNum = a.contains(secretNum);
-                if (!containsSecretNum) {
-                    a.add(secretNum);
-                }
-            } catch (InputMismatchException e) {
-                raw.nextLine();
-            }
-            //
-            line[index] = raw.nextLine();
-            index++;
 
         }
-
-
     }
 }
