0f247af54be43e00004e24400c9915ec4cbff159
mdbubb <mdb17@thebubbs.net>
2017-04-13 22:51:52 -0400

    add em all up

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import org.apache.xerces.impl.xpath.regex.Match;
 
-import java.io.File;
-import java.io.IOException;
+import java.awt.geom.Arc2D;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.NoSuchElementException;
@@ -18,10 +19,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Scanner raw = new Scanner(new File("raw"));
-        raw.nextLine();
         while (raw.hasNext()) {
             try {
+
                 left = raw.nextLine().replaceAll("[^0123456789.+]", "");
+
                 char p[] = left.toCharArray();
                 try {
                     id = "";
@@ -35,14 +37,14 @@ public class Main {
                     StringBuilder d = new StringBuilder(left);
 
                     if (id.length() == 1) {
-                        d.delete(0, 1);
+                        d.delete(0, 2);
                         s = d.toString();
                     } else {
-                        d.delete(0, 2);
+                        d.delete(0, 3);
                         s = d.toString();
 
                     }
-
+                    s= s.replaceAll("\\+", " ");
                     System.out.println(s);
                     //System.out.println(left);
                 } catch (IndexOutOfBoundsException e) {
@@ -52,6 +54,32 @@ public class Main {
                 System.out.println();
             }
 
-        }
+            try {
+
+                        int a = s.indexOf("");
+                        String b = s.substring(a, s.indexOf(" "));
+                        ArrayList<Double> list = new ArrayList<>();
+                        double c = Double.parseDouble(b);
+                        list.add(c);
+
+                        if(s.contains(" ")){
+                            b = s.substring(s.indexOf(" "), s.indexOf(" ")+ 3);
+                            c = Double.parseDouble(b);
+                            list.add(c);
+                            for (int i = 0; i <list.size() ; i++) {
+
+                                System.out.println();
+                                System.out.println(list.get(i));
+                            }
+
+
+                    }
+                } catch (IndexOutOfBoundsException e) {
+                    raw.nextLine();
+                }
+            }
+
+
+
     }
 }
