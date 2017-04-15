93ef28735d8799789ccb27d37cc283a56afcecd0
mdbubb <mdb17@thebubbs.net>
2017-04-14 21:08:54 -0400

    add em all up

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ import java.util.InputMismatchException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.regex.*;
+import java.util.stream.Stream;
 
 /**
  * Created by mdb17 on 4/12/2017.
@@ -16,6 +17,7 @@ public class Main {
     public static String left;
     public static String id;
     public static String s;
+    public static String b[];
 
     public static void main(String[] args) throws IOException {
         Scanner raw = new Scanner(new File("raw"));
@@ -44,8 +46,9 @@ public class Main {
                         s = d.toString();
 
                     }
-                    s= s.replaceAll("\\+", " ");
+                   // s = s.replaceAll("\\+", " ");
                     System.out.println(s);
+                    b = Stream.of(s.split("\\+")).filter(x -> !x.isEmpty()).toArray(String[]::new);
                     //System.out.println(left);
                 } catch (IndexOutOfBoundsException e) {
                     raw.nextLine();
@@ -55,30 +58,35 @@ public class Main {
             }
 
             try {
+                try {
+                    double[] c = new double[b.length];
+                    for (int i = 0; i < b.length; i++) {
+                        c[i] = Double.parseDouble(b[i]);
+                      if (c[i]==0.5||c[i]==1.0){
+                          System.out.println(c[i]);
+                      }
+                      else{
+                          System.out.println();
 
-                        int a = s.indexOf("");
-                        String b = s.substring(a, s.indexOf(" "));
-                        ArrayList<Double> list = new ArrayList<>();
-                        double c = Double.parseDouble(b);
-                        list.add(c);
-
-                        if(s.contains(" ")){
-                            b = s.substring(s.indexOf(" "), s.indexOf(" ")+ 3);
-                            c = Double.parseDouble(b);
-                            list.add(c);
-                            for (int i = 0; i <list.size() ; i++) {
-
-                                System.out.println();
-                                System.out.println(list.get(i));
-                            }
+                          System.out.println(c[i]);
+                          System.out.println();
+                      }
 
 
                     }
-                } catch (IndexOutOfBoundsException e) {
-                    raw.nextLine();
                 }
-            }
+                catch (InputMismatchException d){
+                    raw.nextDouble();
+                }
+                catch (NumberFormatException a){
 
+                    raw.next();
+                }
+
+            } catch (IndexOutOfBoundsException e) {
+                raw.nextLine();
+            }
+        }
 
 
     }
