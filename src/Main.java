601314a9219afc65f3ba0da08e700cd20adab8f4
Bubb, Matt <mdbubb@users.noreply.github.com>
2017-04-23 22:01:29 -0400

    I can see the light at the end of the tunnel and it is gorgeous

--- src/Main.java
+++ src/Main.java
@@ -1,94 +1,149 @@
-import java.io.*;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
-import java.util.stream.Stream;
+import java.util.InputMismatchException;
 
 /**
- * Created by mdb17 on 4/12/2017.
+ * Created by mb977 on 4/20/17.
  */
-public class Main {
-    public static String left;
-    public static String id;
-    public static String s;
-    public static String b[];
-    public static String y[];
-    public static int t = 0;
-    public static double fr = 0.0;
-    public static String a = "";
-    public static double c[];
-    public static double p = 0.0;
-    public static double numoferrors = 0.0;
-    public static double sub;
-    public static Scanner raw;
-    public static ArrayList<String> id1 = new ArrayList<>();
+public class fix {
+    public static ArrayList<Double> totalList = new ArrayList<>();
+    public static studentObjs s;
+    public static double q;
 
-    public static void main(String[] args) throws IOException {
-        ArrayList<String> errornum = new ArrayList<>();
-        ArrayList<String> fr1 = new ArrayList<>();
-        raw = new Scanner(new File("raw"));
-        Scanner sc2 = new Scanner(new File("raw"));
-        while (raw.hasNextLine()) {
+    public static void fix1() {
+        try {
             try {
-                left = raw.nextLine().replaceAll("[^0123456789.+]", "");
-                char p[] = left.toCharArray();
-                try {
-                    id = "";
-                    if (Character.isDigit(p[0])) {
-                        if (Character.isDigit(p[1])) {
-                            id = (p[0] + "" + p[1]);
-                        } else {
-                            id = (p[0] + "");
-                        }
+                Main.c = new double[Main.b.length];
+                for (int i = 0; i < Main.b.length; i++) {
+                    if (Main.b[i].endsWith(".")) {
+                        Main.a = Main.b[i].substring(Main.b[i].length() - 1, Main.b[i].length());
+                        Main.b[i] = Main.b[i].replace(Main.a, "");
                     }
-                    StringBuilder d = new StringBuilder(left);
-                    if (id.length() == 1) {
-                        d.delete(0, 2);
-                        s = d.toString();
-                    } else {
-                        d.delete(0, 3);
-                        s = d.toString();
+                    if (Main.b[i].equals(".5.1") || Main.b[i].equals(".5.0")) {
+                        Main.b[i] = ".5";
+                    } else if (Main.b[i].equals("1.")) {
+                        Main.b[i] = "1";
+                    } else if (Main.b[i].equals(".511.51")) {
+                        Main.b[i] = ".5";
                     }
-                    b = Stream.of(s.split("\\+")).filter(x -> !x.isEmpty()).toArray(String[]::new);
-                } catch (IndexOutOfBoundsException b) {
-                    raw.next();
+                    Main.c[i] = Double.parseDouble(Main.b[i]);
+                    if (Main.c[i] == 222.0) {
+                        Main.c[i] = -1.0;
+                    } else if (Main.c[i] == 0.511) {
+                        Main.c[i] = 0.5;
+                    } else if (Main.c[i] == 11.0) {
+                        Main.c[i] = 1.0;
+                        Main.numoferrors += 1;
+                    } else if (Main.c[i] == 14.0) {
+                        Main.c[i] = 1.0;
+                        Main.numoferrors += 4;
+                    } else if (Main.c[i] == 10.0) {
+                        Main.c[i] = 1.0;
+
+                    } else if (Main.c[i] == 0.51) {
+                        Main.c[i] = 0.5;
+                        Main.numoferrors += 1;
+                    } else if (Main.c[i] == 12.0) {
+                        Main.c[i] = 1.0;
+                        Main.numoferrors += 2;
+
+                    } else if (Main.c[i] == 0.53) {
+                        Main.c[i] = 0.5;
+
+                    } else if (Main.c[i] == 17.0) {
+                        Main.c[i] = 1.0;
+                        Main.numoferrors += 7;
+                    } else if (Main.c[i] == 50.0 || Main.c[i] == 53.0 || Main.c[i] == 5.0) {
+                        Main.c[i] = .5;
+
+                    } else if (Main.c[i] == 132) {
+                        Main.c[i] = 1.0;
+                        Main.numoferrors += 3;
+                    } else if (Main.c[i] == 1.1) {
+                        Main.c[i] = 1.0;
+                        Main.numoferrors += 1;
+                    } else if (Main.c[i] == 0.54) {
+                        Main.c[i] = 0.5;
+                        Main.numoferrors += 4;
+                    } else if (Main.c[i] == 51.0) {
+                        Main.c[i] = 0.5;
+                        Main.numoferrors += 1;
+                    } else if (Main.c[i] == 0.52 || Main.c[i] == 52.0) {
+                        Main.c[i] = 0.5;
+                        Main.numoferrors += 2;
+                    } else if (Main.c[i] == 16.0) {
+                        Main.c[i] = 1;
+                        Main.numoferrors += 6;
+                    } else if (Main.c[i] == 13.0) {
+                        Main.c[i] = 1.0;
+                        Main.numoferrors += 3;
+                    } else if (Main.c[i] == 15.0) {
+                        Main.c[i] = 1.0;
+                        Main.numoferrors += 5;
+                    } else if (Main.c[i] == 5131.0) {
+                        Main.c[i] = 0.5;
+                        Main.numoferrors += 3;
+                    } else if (Main.c[i] == 10.5) {
+                        Main.c[i] = 1.0;
+                        Main.numoferrors += 0.5;
+                    } else if (Main.c[i] == 122.0) {
+                        Main.c[i] = 1.0;
+                        Main.numoferrors += 4;
+                    } else if (Main.c[i] == 112.0) {
+                        Main.c[i] = 1.0;
+                        Main.numoferrors += 1;
+                    } else if (Main.c[i] == 0.5121) {
+                        Main.c[i] = 0.5;
+                        Main.numoferrors += 2;
+                    } else if (Main.c[i] == .5 || Main.c[i] == 1.0) {
+                    }
+                    Main.p += Main.c[i];
+                    Main.sub = Main.numoferrors * .25;
                 }
-            } catch (NoSuchElementException c) {
-                System.out.println();
+                Main.fr = Main.p - Main.sub;
+                s = new studentObjs(Main.id, Main.fr);
+                totalList.add(Main.fr);
+                if (Main.t == 4) {
+                    Main.numoferrors -= 1;
+                } else if (Main.t == 8 || Main.t == 15 || Main.t == 26 || Main.t == 37) {
+                    Main.numoferrors += 1;
+                } else if (Main.t == 21) {
+                    Main.numoferrors += 3;
+                } else if (Main.t == 38) {
+                    Main.numoferrors -= 2;
+                } else if (Main.t == 27 || Main.t == 31 || Main.t == 32 || Main.t == 43 || Main.t == 45) {
+                    Main.numoferrors -= 1;
+                }
+            } catch (InputMismatchException d) {
+                Main.raw.nextDouble();
+            } catch (NumberFormatException l) {
+
+                Main.raw.next();
             }
+            Main.t++;
 
-            p = 0;
-            numoferrors = 0;
-            fix.fix1();
-
-        }
-        while (sc2.hasNextLine()) {
-            y = sc2.nextLine().split("\\t");
-            id1.add(y[0]);
-            fr1.add(y[1]);
-            errornum.add(y[2]);
-        }
-        int count = 1;
-        double n = 0;
-        double two[] = new double[fix.totalList.size()];
-        while (count < id1.size()) {
-
-            String s = fr1.get(count);
-            s = s.replaceAll("[^0123456789.+]", "");
-            String y[] = s.split("\\+");
-            n = Double.parseDouble(errornum.get(count));
-            System.out.println(id1.get(count));
-            try {
-                fix.getFR1(y, n);
-                two[count] = fix.totalList.get(count) - fix.q;
-                System.out.println("FR2: " + two[count]);
-                System.out.println("TOTAL " + fix.totalList.get(count));
-                System.out.println();
-            }//STILL HAVE TO FIGURE THIS CATCH STATEMENT OUT
-            catch (NumberFormatException er) {
-
-            }
-            count++;
+        } catch (IndexOutOfBoundsException e) {
+            Main.raw.nextLine();
         }
     }
+
+    public static void getFR1(String[] dd, double n) {
+        Main.c = new double[dd.length];
+        Main.p = 0.0;
+        for (int i = 1; i < dd.length; i++) {
+            Main.c[i] = Double.parseDouble(dd[i]);
+            Main.p += Main.c[i];
+        }
+        Main.sub = n * .25;
+        q = Main.p - Main.sub;
+        System.out.print("FR1: ");
+        System.out.println(q);
+        Main.p = 0;
+    }
+
+    public static void getAverage() throws IOException {
+
+    }
 }
+
+
