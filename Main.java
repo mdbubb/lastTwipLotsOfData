85f6e6da20cb9a7846b839dc5db51cc4b78b6181
Bubb, Matt <mdbubb@users.noreply.github.com>
2017-05-10 10:27:34 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,12 +3,11 @@ import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.stream.Stream;
-
 /**
  * Created by mdb17 on 4/12/2017.
  */
 public class Main {
-    public static String left;
+    public static String lines;
     public static String id;
     public static String s;
     public static String b[];
@@ -18,7 +17,7 @@ public class Main {
     public static double fr = 0.0;
     public static String a = "";
     public static double c[];
-    public static double p = 0.0;
+   public static double p = 0.0;
     public static double numoferrors = 0.0;
     public static double sub;
     public static Scanner raw;
@@ -32,12 +31,11 @@ public class Main {
         ArrayList<String> fr1 = new ArrayList<>();
         ArrayList<Double> tot = new ArrayList<>();
         raw = new Scanner(new File("raw"));
-        Scanner sc2 = new Scanner(new File("raw"));
         Scanner sc3 = new Scanner(new File("raw"));
         while (raw.hasNextLine()) {
             try {
-                left = raw.nextLine().replaceAll("[^0123456789.+]", "");
-                char p[] = left.toCharArray();
+                lines = raw.nextLine().replaceAll("[^0123456789.+]", "");
+                char p[] = lines.toCharArray();
                 try {
                     id = "";
                     if (Character.isDigit(p[0])) {
@@ -49,7 +47,7 @@ public class Main {
                             id5.add(id);
                         }
                     }
-                    StringBuilder d = new StringBuilder(left);
+                    StringBuilder d = new StringBuilder(lines);
                     if (id.length() == 1) {
                         d.delete(0, 2);
                         s = d.toString();
@@ -68,18 +66,14 @@ public class Main {
             numoferrors = 0;
             fix.fix1();
         }
-        sc3.nextLine();
+       // sc3.nextLine();
         while (sc3.hasNextLine()) {
             y = sc3.nextLine().split("\\t");
             id1.add(y[0]);
             fr1.add(y[1]);
             errornum.add(y[3]);
-            //  System.out.println(y[5]);
         }
-        //sc3.nextLine();
         for (int i = 0; i < fr1.size(); i++) {
-
-
             try {
                 if (!errornum.get(i).isEmpty()) {
 
@@ -88,30 +82,17 @@ public class Main {
             } catch (NumberFormatException t) {
                 errornumdouble.add(0.0);
             }
-
-
-            // System.out.println(i + " " + errornum.get(i));
-            // System.out.println(errornum.get(i));
         }
-
-
         int count = 1;
-        double n = 0;
         double two[] = new double[fix.totalList.size()];
         while (count < id1.size()) {
-
             String s = fr1.get(count);
             s = s.replaceAll("[^0123456789.]", " ");
             String y[] = s.split("\\+");
-//            n = Double.parseDouble(errornum.get(count));
-
-            // fix.getFR2(y, n);
             two[count] = fix.totalList.get(count) - fix.q;
             fr2list.add(two[count]);
             double one[] = new double[7];
-            // double tot[] = new double[56];
             double totalone = 0;
-
             String ji = y[0];
             ji = ji.replaceAll("[^0123456789.]", " ");
             h = ji.split("\\\\s+");
@@ -122,28 +103,22 @@ public class Main {
                 try {
                     one[a] = Double.parseDouble(hi);
                     totalone += one[a];
-                    //   System.out.println(one[a] + "  " + a + "  " + totalone);
+                     // System.out.println(one[a] + "  " + a + "  " + totalone);
                     a++;
-
-
                 } catch (NumberFormatException e) {
 
                 }
-
             }
-
             tot.add(totalone);
-            //    System.out.println(tot.get(count - 1));
             count++;
-
-
+        }
+        for (int j = 0; j <id5.size() ; j++) {
+            System.out.println(id5.get(j));
         }
         tot.add(3.5);
-        for (int i = 0; i < errornumdouble.size(); i++) {
-            System.out.println(id5.get(i) + " FR #1: " + ((tot.get(i)) - ((errornumdouble.get(i) * .25))) + "   FR# 2: " + ((fix.totalList.get(i)) - (tot.get(i)) - ((errornumdouble.get(i) * .25))));
-            // System.out.println(fix.totalList.get(i+1));
+        for (int i = 0; i < errornumdouble.size()+1; i++) {
+            //System.out.println(errornumdouble.get(i));
+          //  System.out.println(id5.get(i) + " FR #1: " + ((tot.get(i)) - ((errornumdouble.get(i+1) * .25))) + "   FR# 2: " + ((fix.totalList.get(i)) - (tot.get(i)) - ((errornumdouble.get(i) * .25))));
         }
-
-
     }
 }
