9782f6b117816042bde838cf19828845335d7d2b
Bubb, Matt <mdbubb@users.noreply.github.com>
2017-05-11 13:34:10 -0400

    Update studentObjs.java

--- src/studentObjs.java
+++ src/studentObjs.java
@@ -1,13 +1,17 @@
 /**
- * Created by mdb17 on 4/17/2017.
+ * Created by mb977 on 5/11/17.
  */
-public class studentObjs {
-    public studentObjs(String id, double total, double one, double two) {
-
+public class studentobjs {
+    public static String i;
+    public studentobjs(String id, double total, double one, double two) {
+        i=id;
 
     }
 
-    public String toString() {
-        return ("ID: " + Main.id + " Total: " + Main.fr);
+    public static String toStrings() {
+
+        return ("ID: " +studentobjs.i + "   Total: " + Main.averageTotal + "   FR#1: " + Main.average + "   FR:2 " + Main.averagetwo);
+
+
     }
 }
